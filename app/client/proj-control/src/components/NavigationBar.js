c526ef41293c137cea3667e203f3d06ce389508d
maki2707 <filip.marcec@fer.hr>
2021-12-13 22:26:25 +0100

    guest i auth linkovi

--- app/client/proj-control/src/components/NavigationBar.js
+++ app/client/proj-control/src/components/NavigationBar.js
@@ -1,6 +1,77 @@
-import React, {Fragment} from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import { toast } from "react-toastify";
 
 function NavigationBar(){
+
+    const logout = async (e) => {
+        e.preventDefault();
+        try {
+          localStorage.removeItem("token");
+          setAuth(false);
+          toast.success("Successfully logged out");
+        } catch (err) {
+          console.error(err.message);
+        }
+      };
+
+    const checkAuthenticated = async () => {
+        try {
+          const res = await fetch("http://localhost:5000/auth/verify", {
+            method: "POST",
+            headers: { jwt_token: localStorage.token }
+          });
+    
+          const parseRes = await res.json();
+    
+          parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+        } catch (err) {
+          console.error('checkAuthenticated error: ', err.message);
+        }
+      };
+    
+      useEffect(() => {
+        checkAuthenticated();
+      }, []);
+    
+      const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+      const setAuth = boolean => {
+        setIsAuthenticated(boolean);
+      };
+    
+
+      const guestLinks = () => (
+        <Fragment> 
+            <div className = 'btn-con'>
+                <a href="/signup" className="anew navlinkother btn btn-2">
+                    Registracija
+                </a>
+            </div>
+
+            <div className="btn-con">
+                <a href="/login" className="anew navlinkother btn btn-2 ">
+                    Prijava
+                </a>
+            </div>
+        </Fragment>
+
+    );
+
+      const authLinks = () => (
+        <Fragment>
+            <div className="btn-con">
+                <a href="#" className="anew navlinkother btn btn-2 ">
+                    Upravljanje računom
+                </a>
+            </div>
+
+            <div onClick={logout}>
+                <a href='/' className="anew navlinkother btn btn-2 ">                
+                    Odjava
+                </a>
+            </div>
+        </Fragment>
+      );
 
     return (
         <Fragment>            
@@ -11,20 +82,9 @@ function NavigationBar(){
                         ProjControl
                     </a>
                 </div>
-            </div>
-            
-            <div className="right-navbar">            
-                <div className = 'btn-con'>
-                    <a href="/signup" className="anew navlinkother btn btn-2">
-                        Registracija
-                    </a>
-                </div>
-
-                <div className="btn-con">
-                    <a href="/login" className="anew navlinkother btn btn-2 ">
-                        Prijava
-                    </a>
-                </div>
+            </div>            
+            <div className="right-navbar">       
+                {isAuthenticated ? authLinks() : guestLinks()}
             </div>
         </nav>
     </Fragment>
