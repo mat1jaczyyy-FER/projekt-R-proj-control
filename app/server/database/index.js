a8333b1ebae7bce962cdb0f7555cee58b530f6d4
maki2707 <filip.marcec@fer.hr>
2021-12-13 22:26:35 +0100

    nove vjerodajnice

--- app/server/database/index.js
+++ app/server/database/index.js
@@ -1,23 +1,17 @@
 const {Pool} = require('pg');
 
-/*const pool = new Pool({
-    user: 'dgmpezensyynio',
-    host: 'ec2-63-33-239-176.eu-west-1.compute.amazonaws.com',
-    database: 'deqh32ufo24qd5',
-    password: '796bd6737567d06b843ac6f378c53cac11682623abe64ac834ea3c972d915ee8',
+const pool = new Pool({
+    user: 'xxlvjmifzfzzxf',
+    host: 'ec2-52-49-23-139.eu-west-1.compute.amazonaws.com',
+    database: 'dcnrusoio98o93',
+    password: 'a9808052bd0227925056275506dfb9d24b1525d73e01d7014adca668b7ecd208',
     port: 5432,
     ssl: {
         rejectUnauthorized: false
     }
-});*/
-
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'projektr-pomocna',
-    password: 'bazepodataka',
-    port: 5432
 });
+
+
 
 module.exports = {
     query: (text, params) => {
